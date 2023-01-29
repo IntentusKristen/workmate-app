40fd2d8a028ea12c3b2e7d8c8061cf0c444dc8c4
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-01-28 20:44:58 -0500

    feat: added text inputs

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,12 +1,21 @@
-import { View, Text, Image, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, Image, StyleSheet, useWindowDimensions } from 'react-native'
+import React, { useState } from 'react'
 import Logo from '../../assets/img/Logo.png'
+import CustomInput from '../components/CustomInput.js'
 
 const SignInScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    // get height of screen to set logo to 30% of screen
+    const {height} = useWindowDimensions()
   return (
     <View style={styles.root}>
-      <Image source={Logo} resizeMode='contain' style={styles.logo}/>
-      <Text>Unlease your potential</Text>
+      <Image source={Logo} resizeMode='contain' style={[styles.logo, {height: height * 0.3}]}/>
+      <Text>Email</Text>
+      <CustomInput placeholder='Your Email' value={email} setValue={setEmail} />
+      <Text>Password</Text>
+      <CustomInput placeholder='Your Password' value={password} setValue={setPassword} secureTextEntry={true}/>
     </View>
   )
 }
@@ -15,12 +24,15 @@ const styles = StyleSheet.create({
     root: {
         alignItems: 'center',
         padding: 20,
+        backgroundColor: '#B0D0D3',
     },
     logo: {
-        // width won't change width for some reason
-        maxWidth: '70%',
-        height: 250,
-    }
+        maxWidth: 300,
+        width: "70%",
+        maxHeight: 200,
+        paddingBottom: 0,
+        marginBottom: 0,
+    },
 })
 
 export default SignInScreen
