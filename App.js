ca8a5df5b84302831dc54fed860ab30f0953b2b8
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-02-08 08:33:46 -0500

    feat: took out navigation in app.js for now

--- App.js
+++ App.js
@@ -1,15 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './src/components/Navigation';
 import SignInScreen from './src/screens/SignInScreen';
-import SignUpScreen from './src/screens/SignUpScreen';
-import ConfirmEmailScreen from './src/screens/ConfirmEmailScreen';
-import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen';
-import NewPasswordScreen from './src/screens/NewPasswordScreen';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ForgotPasswordScreen/>
+      <SignInScreen/>
     </View>
   );
 }
