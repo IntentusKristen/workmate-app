93526a651148a455382bf96be9ad7f9e7713b18b
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-02-12 10:38:02 -0500

    feat: added navigation for all pages

--- App.js
+++ App.js
@@ -36,7 +36,7 @@ export default function App() {
 
   return (
     <View style={{flex: 1}}>
-      <SignUpScreen/>
+      <Navigation/>
     </View>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomePage from '../screens/Welcome';
 import SignInScreen from '../screens/SignInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import ConfirmEmailScreen from '../screens/ConfirmEmailScreen';
@@ -14,8 +15,12 @@ const Navigation = () => {
   return (
     <NavigationContainer>
         <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name='Welcome' component={WelcomePage}></Stack.Screen>
             <Stack.Screen name='SignIn' component={SignInScreen}></Stack.Screen>
-            
+            <Stack.Screen name='SignUp' component={SignUpScreen}></Stack.Screen>
+            <Stack.Screen name='ConfirmEmail' component={ConfirmEmailScreen}></Stack.Screen>
+            <Stack.Screen name='ForgotPassword' component={ForgotPasswordScreen}></Stack.Screen>
+            <Stack.Screen name='NewPassword' component={NewPasswordScreen}></Stack.Screen>
         </Stack.Navigator>
     </NavigationContainer>
   )
