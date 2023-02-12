ad580034204edca66d6a38ec90ffa670259e4e80
Kristen Zhang <85136882+IntentusKristen@users.noreply.github.com>
2023-02-11 23:55:05 -0500

    style: added bold font

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
       await Font.loadAsync({
         'Montserrat-Regular': require('./assets/fonts/Montserrat-Regular.ttf'),
         'Montserrat-Medium': require('./assets/fonts/Montserrat-Medium.ttf'),
-        
+        'Montserrat-Bold': require('./assets/fonts/Montserrat-Bold.ttf')
       });
       setFontLoaded(true);
     }
